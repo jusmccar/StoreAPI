5b516948ad93d035bb9199dc340460c9fc155972
jusmccar <justinmccartney1@yahoo.com>
2022-07-15 13:37:07 -0700

    Add initial app code

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log('04 Store API')
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+const notFound = require("./middleware/not-found");
+const errorHandler = require("./middleware/error-handler");
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("<h1>Store API</h1><a href='/api/v1/products'>products route</a>");
+});
+
+app.use(notFound);
+app.use(errorHandler);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
