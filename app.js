572556814eb7f2da9df776c27a21cb9a65ce68dd
jusmccar <justinmccartney1@yahoo.com>
2022-07-18 12:28:35 -0700

    Add products router

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
+
 const notFound = require("./middleware/not-found");
 const errorHandler = require("./middleware/error-handler");
 
@@ -12,6 +15,8 @@ app.get("/", (req, res) => {
   res.send("<h1>Store API</h1><a href='/api/v1/products'>products route</a>");
 });
 
+app.use("/api/v1/products", productsRouter);
+
 app.use(notFound);
 app.use(errorHandler);
 
@@ -19,6 +24,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`Server is listening on port ${port}...`));
   } catch (error) {
     console.log(error);
