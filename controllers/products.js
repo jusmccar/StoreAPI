0027d9efeca1adce31990b4282b304bef8e1c42f
jusmccar <justinmccartney1@yahoo.com>
2022-07-19 10:37:33 -0700

    Add fields

--- controllers/products.js
+++ controllers/products.js
@@ -10,7 +10,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -33,6 +33,11 @@ const getAllProducts = async (req, res) => {
     result = result.sort(sortList);
   } else {
     result = result.sort("createdAt");
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
   }
 
   const products = await result;
